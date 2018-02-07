579b9e65b730af59c9106a81db07a270ff4a5b9b
Daniel Muey <dan@cpanel.net>
2018-02-07 09:19:27 -0600

    ZC-3382: use explicit pid file

--- SOURCES/setenv.sh
+++ SOURCES/setenv.sh
@@ -1,3 +1,2 @@
 export CATALINA_OPTS="$CATALINA_OPTS -server -Dfile.encoding=UTF-8 -Xms128m -Xmx6248m"
-# TODO: We'll need this to place the pid file
-# export CATALINA_PID="$CATALINA_BASE/bin/catalina.pid"
+export CATALINA_PID="/var/run/catalina.pid"
